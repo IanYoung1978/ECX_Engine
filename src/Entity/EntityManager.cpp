80ae59d0de27023356bd75478f7300ebd1fbcd95
ianyoung1978 <i.young@ymail.com>
2025-03-11 20:07:46 +0000

    1: Added Entity manager singleton. Added search entity by typeID

---
+++ src/Entity/EntityManager.cpp
@@ -0,0 +1,121 @@
+#include "EntityManager.h"
+#include "Logging/ECX_Logging.h"
+
+EntityManager::EntityManager()
+{
+}
+
+EntityManager::~EntityManager()
+{
+}
+
+
+EntityManager& EntityManager::getInstance()
+{
+	if (m_instance == nullptr)
+	{
+		m_instance = new EntityManager();
+	}
+	return *m_instance;
+}
+
+void EntityManager::addEntity(std::shared_ptr<GameEntity> entity)
+{
+	std::scoped_lock<std::mutex> lock(m_lock);
+	m_entities.push_back(entity);
+}
+
+void EntityManager::removeEntity(std::shared_ptr<GameEntity> entity)
+{
+	std::scoped_lock<std::mutex> lock(m_lock);
+	auto it = std::find(m_entities.begin(), m_entities.end(), entity);
+	if (it != m_entities.end())
+	{
+		m_entities.erase(it);
+	}
+	else
+	{
+		// Entity not found	
+		LOGGING::ECX_Logger::GetInstance()->LogMessage("Entity not found", LOGGING::LogLevel::SEVERE);
+	}
+}
+
+void EntityManager::removeEntity(std::string name)
+{
+	std::scoped_lock<std::mutex> lock(m_lock);
+	auto it = std::find_if(m_entities.begin(), m_entities.end(), [name](std::shared_ptr<GameEntity> entity) { return entity->getName() == name; });
+	if (it != m_entities.end())
+	{
+		m_entities.erase(it);
+	}
+	else
+	{
+		// Entity not found	
+		LOGGING::ECX_Logger::GetInstance()->LogMessage("Entity not found", LOGGING::LogLevel::SEVERE);
+	}
+}
+
+std::shared_ptr<GameEntity> EntityManager::getEntity(std::string name)
+{
+	std::scoped_lock<std::mutex> lock(m_lock);
+	auto it = std::find_if(m_entities.begin(), m_entities.end(), [name](std::shared_ptr<GameEntity> entity) { return entity->getName() == name; });
+	if (it != m_entities.end())
+	{
+		return *it;
+	}
+
+	// Entity not found	
+	LOGGING::ECX_Logger::GetInstance()->LogMessage("Entity not found", LOGGING::LogLevel::SEVERE);
+
+	return nullptr;
+}
+
+std::vector<std::shared_ptr<GameEntity>>& EntityManager::getEntities()
+{
+	std::scoped_lock<std::mutex> lock(m_lock);
+	return m_entities;
+}
+
+std::vector<std::shared_ptr<GameEntity>> EntityManager::getEntitiesWithComponent(std::type_index type)
+{
+	std::scoped_lock<std::mutex> lock(m_lock);
+	std::vector<std::shared_ptr<GameEntity>> entities;
+	for (auto entity : m_entities)
+	{
+		if (entity->hasComponent(type))
+		{
+			entities.push_back(entity);
+		}
+	}
+	return entities;
+}
+
+std::vector<std::shared_ptr<GameEntity>> EntityManager::getEntitiesWithComponent(std::vector<std::type_index> types)
+{
+	std::scoped_lock<std::mutex> lock(m_lock);
+	std::vector<std::shared_ptr<GameEntity>> entities;
+	for (auto entity : m_entities)
+	{
+		bool hasAllComponents = true;
+		for (auto type : types)
+		{
+			if (!entity->hasComponent(type))
+			{
+				hasAllComponents = false;
+				break;
+			}
+		}
+		if (hasAllComponents)
+		{
+			entities.push_back(entity);
+		}
+	}
+	return entities;
+}
+
+void EntityManager::clearEntities()
+{
+	m_entities.clear();
+}
+
+EntityManager* EntityManager::m_instance = nullptr;
---
+++ src/Entity/EntityManager.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "GameEntity.h"
+#include <vector>
+#include <memory>
+#include <mutex>
+
+
+class EntityManager
+{
+public:
+	EntityManager(const EntityManager&) = delete;
+	EntityManager& operator=(const EntityManager&) = delete;
+	static EntityManager& getInstance();
+	void addEntity(std::shared_ptr<GameEntity> entity);
+	void removeEntity(std::shared_ptr<GameEntity> entity);
+	void removeEntity(std::string name);
+	void clearEntities();
+	std::shared_ptr<GameEntity> getEntity(std::string name);
+	std::vector<std::shared_ptr<GameEntity>>& getEntities();
+	std::vector<std::shared_ptr<GameEntity>> getEntitiesWithComponent(std::type_index type);
+	std::vector<std::shared_ptr<GameEntity>> getEntitiesWithComponent(std::vector<std::type_index> types);
+	virtual ~EntityManager();
+private:
+	EntityManager();
+	std::vector<std::shared_ptr<GameEntity>> m_entities;
+	std::mutex m_lock;
+	static EntityManager* m_instance;
+};
